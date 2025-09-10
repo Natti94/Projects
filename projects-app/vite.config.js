d4be0b50e76520f7bdd8df0734d4b58ebd14e665
Natti94 <natnael.berhane.sv@gmail.com>
2025-09-10 10:22:35 +0200

    update

--- projects-app/vite.config.js
+++ projects-app/vite.config.js
@@ -4,9 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.VITE_PORT,
     proxy: {
-      "/api": `http://localhost:${process.env.VITE_PORT + 1}`,
+      "/api": "http://localhost:3001", 
     },
   },
 });
